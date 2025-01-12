ad11f787bb8db89b0f44f1a6f234ff6dc34a26c0
Tom-the-Bomb <77308095+Tom-the-Bomb@users.noreply.github.com>
2025-01-12 00:03:23 -0500

    z

--- vite.config.js
+++ vite.config.js
@@ -1,13 +1,7 @@
 import { defineConfig } from 'vite';
-import { ViteImageOptimizer } from 'vite-plugin-image-optimizer';
 import * as path from 'path';
 
 export default defineConfig({
-    plugins: [
-        ViteImageOptimizer({
-            jpeg: { quality: 70 },
-        })
-    ],
     resolve: {
         alias: {
             '@assets': path.resolve(__dirname, 'src/assets'),
