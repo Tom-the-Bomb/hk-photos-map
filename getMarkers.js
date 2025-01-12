4d674566ccb60ed1bd58872837c9f7705da1a1cb
Tom-the-Bomb <77308095+Tom-the-Bomb@users.noreply.github.com>
2025-01-12 14:34:14 -0500

    add markers json cache

---
+++ getMarkers.js
@@ -0,0 +1,36 @@
+
+import fs from 'fs';
+import exifr from 'exifr';
+
+const DIRS = [
+    './src/assets/mtr/',
+    './src/assets/others/',
+];
+
+async function readGPSData() {
+    const markers = {};
+
+    for (const dir of DIRS) {
+        const images = fs.readdirSync(dir);
+
+        for (let image of images) {
+            image = dir + image;
+
+            const {latitude, longitude} = await exifr.gps(image);
+            const coord = JSON.stringify([latitude, longitude]);
+
+            if (markers[coord] == null) {
+                markers[coord] = [];
+            }
+
+            markers[coord].push(image.replace('./src', '.'));
+        }
+    }
+    return markers;
+}
+
+readGPSData().then((markers) => {
+    console.log('[Successfully read GPS data]');
+    fs.writeFileSync('./src/assets/markers.json', JSON.stringify(markers));
+    console.log('[Successfully recorded GPS data]');
+});
--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,6 @@
 import L from 'leaflet';
 import 'leaflet/dist/leaflet.css';
 
-import exifr from 'exifr';
-
 import Swiper from 'swiper';
 import { Keyboard, EffectCoverflow } from 'swiper/modules';
 
@@ -11,14 +9,12 @@ import 'swiper/swiper-bundle.css';
 import { getImageHTML, getMarker } from './utils';
 import './style.css';
 
-import orangeMarkerAsset from '@assets/orange-marker.png';
-import mtrMarkerAsset from '@assets/mtr-marker.png';
+import orangeMarkerAsset from './assets/orange-marker.png';
+import mtrMarkerAsset from './assets/mtr-marker.png';
+
+import markers from './assets/markers.json';
 
 let swiper: Swiper | null = null;
-
-const images: string[] = Object.values(import.meta.glob('@assets/**/*.avif', { eager: true, query: '?url', import: 'default' }));
-console.log(images);
-const markers = new Map<string, string[]>();
 
 const orangeMarker = getMarker(orangeMarkerAsset);
 const mtrMarker = getMarker(mtrMarkerAsset);
@@ -46,107 +42,91 @@ L.tileLayer(
 )
     .addTo(map);
 
-async function sortGPSData() {
-    for (const image of images) {
-        const {latitude, longitude} = await exifr.gps(image);
-        console.log('AAAAA');
-        const coord = JSON.stringify([latitude, longitude]);
+for (const [coord, images] of Object.entries(markers)) {
+    const [latitude, longitude] = JSON.parse(coord);
+    const cover = images[0];
 
-        if (!markers.has(coord)) {
-            markers.set(coord, []);
-        }
+    L.marker([latitude, longitude], {icon: cover.includes('mtr') ? mtrMarker : orangeMarker})
+        .addTo(map)
+        .bindPopup(
+            `<img class="cover" src="${cover}" alt="${cover}"/>
+            <p class="cover-caption">${images.length} photo${images.length > 1 ? 's' : ''}</p>`
+        )
+        .on('click', (event) => {
+            const popUp = event.target
+                .getPopup()
+                .openPopup();
 
-        markers.get(coord)!.push(image);
-    }
+            const popUpElement = popUp.getElement();
+
+            popUpElement.addEventListener('click', async () => {
+                map.dragging.disable();
+                map.touchZoom.disable();
+                map.doubleClickZoom.disable();
+                map.scrollWheelZoom.disable();
+                map.boxZoom.disable();
+                map.keyboard.disable();
+
+                if (images.length > 1) {
+                    document.getElementById('gallery')!.innerHTML = `
+                        <div class="swiper">
+                            <div class="swiper-wrapper">
+                                ${
+                                    (await Promise.all(
+                                        images.map(async (image) =>
+                                            `<div class="swiper-slide swiper-slide-styles">
+                                                ${await getImageHTML(image)}
+                                            </div>`
+                                        )
+                                    ))
+                                        .join('')
+                                }
+                            </div>
+                        </div>`;
+
+                    swiper = new Swiper('.swiper', {
+                        modules: [Keyboard, EffectCoverflow],
+                        effect: 'coverflow',
+                        loop: true,
+                        grabCursor: true,
+                        centeredSlides: true,
+                        keyboard: true,
+                        direction: 'vertical',
+                        breakpoints: {
+                            768: {
+                                direction: 'horizontal',
+                            },
+                        },
+                    });
+                } else {
+                    gallery.innerHTML =
+                        `<div class="swiper-slide-styles">
+                            ${await getImageHTML(images[0])}
+                        </div>`;
+                }
+
+                galleryWrapper.style.display = 'block';
+            });
+
+            popUpElement.querySelector('.leaflet-popup-close-button')
+                ?.addEventListener('click', (event: Event) => {
+                    event.stopPropagation();
+                });
+        });
 }
 
-sortGPSData().then(() => {
-    for (const [coord, images] of markers) {
-        const [latitude, longitude] = JSON.parse(coord);
-        const cover = images[0];
+document.getElementById('gallery-close-btn')?.addEventListener('click', () => {
+    gallery.innerHTML = '';
+    galleryWrapper.style.display = 'none';
 
-        L.marker([latitude, longitude], {icon: cover.includes('mtr') ? mtrMarker : orangeMarker})
-            .addTo(map)
-            .bindPopup(
-                `<img class="cover" src="${cover}" alt="${cover}"/>
-                <p class="cover-caption">${images.length} photo${images.length > 1 ? 's' : ''}</p>`
-            )
-            .on('click', (event) => {
-                const popUp = event.target
-                    .getPopup()
-                    .openPopup();
-
-                const popUpElement = popUp.getElement();
-
-                popUpElement.addEventListener('click', async () => {
-                    map.dragging.disable();
-                    map.touchZoom.disable();
-                    map.doubleClickZoom.disable();
-                    map.scrollWheelZoom.disable();
-                    map.boxZoom.disable();
-                    map.keyboard.disable();
-
-                    if (images.length > 1) {
-                        document.getElementById('gallery')!.innerHTML = `
-                            <div class="swiper">
-                                <div class="swiper-wrapper">
-                                    ${
-                                        (await Promise.all(
-                                            images.map(async (image) =>
-                                                `<div class="swiper-slide swiper-slide-styles">
-                                                    ${await getImageHTML(image)}
-                                                </div>`
-                                            )
-                                        ))
-                                            .join('')
-                                    }
-                                </div>
-                            </div>`;
-
-                        swiper = new Swiper('.swiper', {
-                            modules: [Keyboard, EffectCoverflow],
-                            effect: 'coverflow',
-                            loop: true,
-                            grabCursor: true,
-                            centeredSlides: true,
-                            keyboard: true,
-                            direction: 'vertical',
-                            breakpoints: {
-                                768: {
-                                    direction: 'horizontal',
-                                },
-                            },
-                        });
-                    } else {
-                        gallery.innerHTML =
-                            `<div class="swiper-slide-styles">
-                                ${await getImageHTML(images[0])}
-                            </div>`;
-                    }
-
-                    galleryWrapper.style.display = 'block';
-                });
-
-                popUpElement.querySelector('.leaflet-popup-close-button')
-                    ?.addEventListener('click', (event: Event) => {
-                        event.stopPropagation();
-                    });
-            });
+    if (swiper != null) {
+        swiper.destroy();
     }
 
-    document.getElementById('gallery-close-btn')?.addEventListener('click', () => {
-        gallery.innerHTML = '';
-        galleryWrapper.style.display = 'none';
-
-        if (swiper != null) {
-            swiper.destroy();
-        }
-
-        map.dragging.enable();
-        map.touchZoom.enable();
-        map.doubleClickZoom.enable();
-        map.scrollWheelZoom.enable();
-        map.boxZoom.enable();
-        map.keyboard.enable();
-    });
+    map.dragging.enable();
+    map.touchZoom.enable();
+    map.doubleClickZoom.enable();
+    map.scrollWheelZoom.enable();
+    map.boxZoom.enable();
+    map.keyboard.enable();
 });
