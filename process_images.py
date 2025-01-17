3f041d7933c208dea33ade35b2f2674e185685f4
Tom-the-Bomb <77308095+Tom-the-Bomb@users.noreply.github.com>
2025-01-17 00:19:02 -0500

    clean up `process_images.py`

--- process_images.py
+++ process_images.py
@@ -78,37 +78,5 @@ def compress(
             remove(file)
 
 if __name__ == '__main__':
-    # process_heif('./src/assets/', quality=100)
-    # add_gps([
-    #     # central station (overwrite)
-    #     (r'src\assets\mtr\mtr-20241231_025035206_iOS.avif', """22°16'55.7"N 114°09'27.9"E"""),
-    #     # monster building spot (overwrite)
-    #     (r'src\assets\others\20250101_035300000_iOS.avif', """22°17'03.2"N 114°12'43.8"E"""),
-
-    #     # sheung wan station
-    #     (r'src/assets/mtr/20250101_114816926_iOS.jpg', """22°17'11.6"N 114°09'06.6"E"""),
-    #     # sun yat sen park
-    #     (r'src\assets\others\20241230_052400000_iOS.jpg', """22°17'25.4"N 114°08'45.2"E"""),
-    #     # 1881 heritage (2)
-    #     (r'src\assets\others\20241231_052000000_iOS.jpg', """22°17'41.5"N 114°10'12.4"E"""),
-    #     # the peninsula (re-edited)
-    #     (r'src/assets/others/20241231_063800000_iOS.jpg', """22°17'40.5"N 114°10'18.7"E"""),
-    #     # King's road underpass (new image)
-    #     (r'src\assets\others\20250101_040200000_iOS.jpg', """22°17'12.7"N 114°12'37.1"E"""),
-    #     # western market (re-edited)
-    #     (r'src\assets\others\20250101_120425700_iOS.jpg', """22°17'15.2"N 114°09'01.2"E"""),
-    #     # water street sign (2) (street)
-    #     (r'src\assets\others\20250102_022453300_iOS.jpg', """22°17'15.2"N 114°08'19.7"E"""),
-    #     # wilmer street market (re-edited)
-    #     (r'src\assets\others\20250102_023724300_iOS.jpg', """22°17'16.0"N 114°08'41.6"E"""),
-    #     # courtyard marriott
-    #     (r'src\assets\others\20250103_011442000_iOS.jpg', """22°17'16.8"N 114°08'19.4"E"""),
-    #     # tung chung from HKG T1
-    #     (r'src\assets\others\20250103_043427500_iOS.jpg', """22°18'50.9"N 113°56'08.1"E"""),
-    #
-    #     # star ferry (re-edited)
-    #     (r'src/assets/others/20241231_033000000_iOS.jpg', """22°17'11.8"N 114°09'40.6"E"""),
-    #     # HKU (re-edited)
-    #     (r'src/assets/others/20250101_025400000_iOS.jpg', """22°17'04"N 114°08'17"E"""),
-    # ])
+    process_heif('./src/assets/')
     compress('./src/assets/', quality=70)
